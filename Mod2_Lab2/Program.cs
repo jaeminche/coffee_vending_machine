6e5c422a502413956a320d2ea92f5a7f4bf98ded
jaeminche <jaeminche@gmail.com>
2017-09-01 14:48:08 +0900

    Make a code for Coffee vending machine using switch!

--- Mod2_Lab2/Program.cs
+++ Mod2_Lab2/Program.cs
@@ -10,6 +10,36 @@ namespace Mod2_Lab2
     {
         static void Main(string[] args)
         {
+            // Create a switch block   (A CODE FOR COFFEE VENDING MACHINE!!!!!!!!!!!)
+
+            Console.WriteLine("Coffee sizes: 1=small 2=medium 3=large");
+            Console.WriteLine("Please enter your selection: ");
+            string str = Console.ReadLine();
+            int cost = 0;
+
+            switch (str)
+            {
+                case "1":
+                case "small":
+                    cost += 25;
+                    break;
+                case "2":
+                case "medium":
+                    cost += 50;
+                    break;
+                case "3":
+                case "large":
+                    cost += 75;
+                    break;
+                default:
+                    Console.WriteLine("Invalid selection. Please select 1, 2, or 3.");
+                    break;
+            }
+            if (cost != 0)
+            {
+                Console.WriteLine("Please insert {0} cents.", cost);
+            }
+            Console.WriteLine("Thank you for your business.");
         }
     }
 }
